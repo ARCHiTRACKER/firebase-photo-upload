6ddf46995d467026c015a7a3771491342cb44e84
Max <maxzitron@gmail.com>
2017-01-27 08:51:06 +1100

    Init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Firebase upload
+
+### This script require `Node.js` in order to run
+
+## To run the script script please follow the instruction
+1. Run npm install
+    ```sh
+    npm install
+    ```
+2. Copy all photos that you want to upload to photo/
+3. Run upload script
+    ```sh
+    npm run upload
+    ```
+4. The result would be appreared in output.csv
---
+++ index.js
@@ -0,0 +1,34 @@
+// node component import
+const gcloud = require('google-cloud');
+var recursive = require('recursive-readdir');
+
+
+const storage = gcloud.storage({
+    projectId: 'architracker-1484317434204',
+    keyFilename: './ARCHiTRACKER-e877c8b07a42.json',
+});
+
+const bucket = storage.bucket('architracker-1484317434204.appspot.com');
+
+// make sure that the bucket is exists
+bucket.exists(function(err, exists) {
+
+    if(exists) {
+        recursive('photos',['.DS_Store'],function (err, files) {
+            var currentPath = [];
+
+            files.map(function (filePath) {
+                var fileName = filePath.split('/');
+                fileName = fileName[fileName.length-1];
+
+                    // upload file
+                bucket.upload(filePath, {destination: filePath, public: true}, function (err, file) {
+                    if (!err) {
+                        var public_path = 'https://storage.googleapis.com/architracker-1484317434204.appspot.com/' + filePath;
+                        console.log(fileName+","+public_path);
+                    }
+                });
+            });
+        });
+    }
+});
